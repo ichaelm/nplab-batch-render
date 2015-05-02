fb6a77523110865801b47bae61f5e1f32d306a03
Michael Schaffer <mls278@case.edu>
2015-05-01 22:24:31 -0400

    batch_render passes MXS paths to threads via a temp file

--- src/batch_render.sh
+++ src/batch_render.sh
@@ -3,12 +3,12 @@
 # batch_render.sh
 # 
 # usage:
-# ./batch_render.sh MXS_DIR OUTPUT_DIR [PROJECT_NAME]
+# ./batch_render.sh MXS_DIR OUTPUT_DIR NUM_NODES [PROJECT_NAME]
 # 
 # This script finds all the .mxs files in MXS_DIR, renders all of them with
-# Maxwell Renderer using as many cluster jobs as it can without exceeding the
-# license limits, and outputs the resulting png images to OUTPUT_DIR with the
-# same names, plus diagnostic output and error log files.
+# Maxwell Renderer using as many cluster jobs as it can without exceeding
+# NUM_NODES, and outputs the resulting png images to OUTPUT_DIR with the same
+# names, plus diagnostic output and error log files.
 
 set -o nounset
 set -o errexit
@@ -19,33 +19,32 @@ OUTPUT_DIR=$2
 NUM_NODES=$3
 PROJNAME=${4-render} #optional
 
-# function initializes variables for keeping track of file name list and thread
-# number
-InitFileNameList () {
-  FILE_NAME_LIST=""
-  NUM_FILE_NAMES=0
-  THREAD_NUM=0
+# global variables
+THREAD_NUM=0
+
+# function initializes temp file that lists MXS file paths
+InitMXSFileList () {
+  MXS_FILE_LIST_PATH=$(mktemp tmp_batch_render.XXXXXXXXXX) || { echo "Failed to create temp file"; exit 1; }
+  NUM_MXS_FILES_LISTED=0
 }
 
-# function adds the parameter to the file name list
-AddToFileNameList () {
-  FILE_NAME_LIST="$1:${FILE_NAME_LIST}"
-  NUM_FILE_NAMES=$((NUM_FILE_NAMES+1))
+# function adds the parameter to the MXS file path list
+AddToMXSFileList () {
+  echo $1 >> $MXS_FILE_LIST_PATH
+  NUM_MXS_FILES_LISTED=$((NUM_MXS_FILES_LISTED+1))
 }
 
 # function starts a job to render the current list of files using Maxwell,
 # clears the list and increments the thread number
 StartRenderJob () {
   THREAD_NUM=$((THREAD_NUM+1))
-  echo "Starting thread $THREAD_NUM with FILE_NAME_LIST = $FILE_NAME_LIST"
+  echo "Starting thread $THREAD_NUM with $NUM_MXS_FILES_LISTED files"
   qsub -N "${PROJNAME}_thread_${THREAD_NUM}"  -l walltime=23:00:00 \
     -l nodes=1:ppn=12,mem=10gb \
     -o ${OUTPUT_DIR}/${PROJNAME}_thread_${THREAD_NUM}_output.txt \
     -e ${OUTPUT_DIR}/${PROJNAME}_thread_${THREAD_NUM}_error.txt \
-    -v MXS_DIR=${MXS_DIR},MXS_NAMES=${FILE_NAME_LIST},OUTPUT_DIR=${OUTPUT_DIR} \
+    -v MXS_FILE_LIST_PATH=${MXS_FILE_LIST_PATH},OUTPUT_DIR=${OUTPUT_DIR} \
     ./render_thread.pbs
-  FILE_NAME_LIST=""
-  NUM_FILE_NAMES=0
 }
 
 # init log
@@ -53,18 +52,18 @@ echo "Running batch_render.sh"
 echo "Started $(date +%F\ %T)"
 
 # calculated constants
-NUM_FILES=$(find ${MXS_DIR}/*.mxs -maxdepth 0 -type f | wc -l)
-NUM_FILES_PER_NODE=$(((NUM_FILES + (NUM_NODES-1))/NUM_NODES))
+NUM_MXS_FILES=$(find ${MXS_DIR}/*.mxs -maxdepth 0 -type f | wc -l)
+NUM_MXS_FILES_PER_NODE=$(((NUM_MXS_FILES + (NUM_NODES-1))/NUM_NODES))
 
 # log info
 echo "MXS_DIR = $MXS_DIR"
 echo "OUTPUT_DIR = $OUTPUT_DIR"
 echo "NUM_NODES = $NUM_NODES"
 echo "PROJNAME = $PROJNAME"
-echo "NUM_FILES = $NUM_FILES"
-echo "NUM_FILES_PER_NODE = $NUM_FILES_PER_NODE"
+echo "NUM_MXS_FILES = $NUM_MXS_FILES"
+echo "NUM_MXS_FILES_PER_NODE = $NUM_MXS_FILES_PER_NODE"
 
-InitFileNameList
+InitMXSFileList
 
 # create output directory if necessary
 if [ ! -d ${OUTPUT_DIR} ]
@@ -75,21 +74,24 @@ fi
 # iterate over each mxs file
 for MXS_PATH in ${MXS_DIR}/*.mxs
 do 
-  temp=${MXS_PATH##*/}
-  MXS_NAME=${temp%.*}  
+  #temp=${MXS_PATH##*/}
+  #MXS_NAME=${temp%.*}  
 
-  AddToFileNameList ${MXS_NAME}
+  AddToMXSFileList ${MXS_PATH}
 
-  if [ $NUM_FILE_NAMES -eq $NUM_FILES_PER_NODE ]
+  if [ $NUM_MXS_FILES_LISTED -eq $NUM_MXS_FILES_PER_NODE ]
   then
     StartRenderJob
+    InitMXSFileList
   fi
 done
 
 # final job for leftover files
-if [ $NUM_FILE_NAMES -gt 0 ]
+if [ $NUM_MXS_FILES_LISTED -gt 0 ]
 then
   StartRenderJob
+else
+  rm ${MXS_FILE_LIST_PATH}
 fi
 
 echo "Ended $(date +%F\ %T)"
