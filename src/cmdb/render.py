759295551023e3a8de5c71e1ebc3243c4710d12c
Michael Schaffer <mls278@case.edu>
2015-05-29 12:55:36 -0400

    Make render.py constantly query for the next row, for concurrency

--- src/cmdb/render.py
+++ src/cmdb/render.py
@@ -2,6 +2,7 @@ import sys
 import sqlite3
 import subprocess
 import os #remove
+import time
 from cmdb import files
 
 def main(main_dir):
@@ -12,8 +13,12 @@ def main(main_dir):
     
     main_dir = files.ensure_suffix(main_dir, '/')
     
-    cursor.execute('''SELECT configID, scene, camera, target, trace, frame, frameID FROM Scenes NATURAL JOIN CameraTargets NATURAL JOIN Traces NATURAL JOIN Frames WHERE hasFrameMXS != 0 AND hasImage = 0''')
-    for conf, scene, camera, target, trace, frame, frameID in cursor.fetchall():
+    while True:
+        cursor.execute('''SELECT configID, scene, camera, target, trace, frame, frameID FROM Scenes NATURAL JOIN CameraTargets NATURAL JOIN Traces NATURAL JOIN Frames WHERE hasFrameMXS != 0 AND hasImage = 0 LIMIT 1''')
+        t = cursor.fetchone()
+        if not t:
+            break;
+        (conf, scene, camera, target, trace, frame, frameID) = t
         mxs_dir = files.get_scene_mxs_dir(main_dir, scene)
         framemxs_path = files.get_frame_mxs_path(main_dir, conf, scene, camera, target, trace, frame)
         image_path = files.get_image_path(main_dir, conf, scene, camera, target, trace, frame)
@@ -23,9 +28,12 @@ def main(main_dir):
                          '-output:' + image_path, '-dep:' + mxs_dir,
                          '-dep:"/usr/local/maxwell-3.0/materials database/textures"',])
         image_info = files.get_image_info(main_dir, conf, scene, camera, target, trace, frame)
+        start = time.time()
         conn.execute('''UPDATE Frames SET hasImage = ?, imageLastModified = ?, imageHash = ? WHERE frameID = ?''', (image_info.hash != None, image_info.last_modified, image_info.hash, frameID))
         conn.commit()
+        print("elapsed: " + str(time.time() - start))
     conn.close()
+    print("exited normally")
 
 if __name__ == "__main__":
     main(sys.argv[1])
